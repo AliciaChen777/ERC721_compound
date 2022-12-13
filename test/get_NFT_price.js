0a0c161975489bacf6adbc00a0376b3db4b8e818
alicianingchen <40305367+alicianingchen@users.noreply.github.com>
2022-12-13 20:35:58 +0800

    successfully supply ctokenNFT

--- test/get_NFT_price.js
+++ test/get_NFT_price.js
@@ -14,9 +14,8 @@ describe("compound", async function () {
     // get admin addresss
     let owner, addr1;
     let nftChainLink;
-    let azuki_holder = '0x5d7aaa862681920ea4f350a670816b0977c80b37';
-    //const nft_addr = "0x21f73D42Eb58Ba49dDB685dc29D3bF5c0f0373CA"
-    let azuki_nft = '0xED5AF388653567Af2F388E6224dC7C4b3241C544'
+    let clonex_holder = '0xbad1990c2967231bc9a4fa9562ea68e65dd2b25d';
+    let clonex_nft = '0x49cF6f5d44E70224e2E23fDcdd2C053F30aDA28B'
     let decimals = 18;
     it("TEST Mint", async function () {
         [owner, addr1] = await ethers.getSigners();
@@ -56,18 +55,18 @@ describe("compound", async function () {
         comptroller = await Comptroller.deploy();
 
         // //create oracel
-        // let Oracle = await ethers.getContractFactory("SimplePriceOracle");
-        // oracle = await Oracle.deploy();
+        let Oracle = await ethers.getContractFactory("SimplePriceOracle");
+        oracle = await Oracle.deploy();
 
 
 
 
 
         cTokenNFT = await cerc721.deploy(
-            azuki_nft,
+            clonex_nft,
             comptroller.address,
             interestRateModel.address,
-            2,
+            1,
             'NFT_name',
             'NFT_symbol',
             decimals,
@@ -76,29 +75,52 @@ describe("compound", async function () {
         await cTokenNFT.deployed();
         console.log('cTokenNFT addr', cTokenNFT.address)
 
-        let tokenId = 7938
-        let azuki_instance = await ethers.getContractAt("ERC721", azuki_nft)
-        await impersonateAccount(azuki_holder);
-        azuki = await ethers.getSigner(azuki_holder);
-        console.log('azuki.address', azuki.address)
-        //轉azuki給owner，讓owner抵押
-        await azuki_instance.connect(azuki).transferFrom(azuki.address, owner.address, tokenId);
+        let tokenId = 9791
+        let clonex_instance = await ethers.getContractAt("ERC721", clonex_nft)
+        await impersonateAccount(clonex_holder);
+        clonex_signer = await ethers.getSigner(clonex_holder);
+        console.log('clonex_signer.address', clonex_signer.address)
+        //轉clonex給owner，讓owner抵押
+        await clonex_instance.connect(clonex_signer).transferFrom(clonex_signer.address, owner.address, tokenId);
         //已成功轉給owner
-        console.log("azuki_instance.ownerOf(tokenId)", await azuki_instance.ownerOf(tokenId))
+        console.log("clonex_instance.ownerOf(tokenId)", await clonex_instance.ownerOf(tokenId))
 
-        //owner提交azuki_nft給compound 成為抵押品
-        //await cTokenNFT.connect(owner).mint_721(USDCAmount);
-
-
-
-
-
-
+        //qpprove clonex instance 準備給ctoken
+        // approve 給cerc721合約調用
+        // cerc721 address: 0x2bb8b93f585b43b06f3d523bf30c203d3b6d4bd4
+        //因為是用immutable布合約的，沒辦法直接cerc721.address
+        await clonex_instance.approve('0x2bb8b93f585b43b06f3d523bf30c203d3b6d4bd4', tokenId);
+        console.log("finish approve");
 
 
 
 
+        let tokenNFTPrice = BigInt(100 * 1e18);
+        let collateralFactorNFT = BigInt(0.5 * 1e18);
+        //liquidate factor
+        let closeFactor = BigInt(0.5 * 1e18);
+        //await oracle.setUnderlyingPrice(cTokenA.address, tokenAPrice);
 
+        await oracle.setUnderlyingPrice(cTokenNFT.address, tokenNFTPrice);
+        //support market
+        //await comptroller._supportMarket(cTokenA.address);
+        await comptroller._supportMarket(cTokenNFT.address);
+        //set oracle
+        await comptroller._setPriceOracle(oracle.address);
+        //set collateral
+        //await comptroller._setCollateralFactor(cTokenA.address, collateralFactorA);
+        await comptroller._setCollateralFactor(cTokenNFT.address, collateralFactorNFT);
+        // set close factor
+        await comptroller._setCloseFactor(closeFactor);
+        // set liquidation incentive
+        //await comptroller._setLiquidationIncentive(liquidationIncentive);
+        // token B
+
+
+
+        //owner提交clonex_nft給compound 成為抵押品
+        await cTokenNFT.connect(owner).mint_721(tokenId, 1);
+        console.log('await cTokenNFT.balanceOf(owner.address) =', await cTokenNFT.balanceOf(owner.address))
 
     })
 
